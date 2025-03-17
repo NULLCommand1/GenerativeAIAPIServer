d6dfe59af4ae9a605a82caf705b72fefe2d570c0
NULLCommnand1 <nullcommand1@gmail.com>
2025-03-18 02:46:19 +0700

    refactoring: remove cluster logic and simplify server startup for context lock data async reasons, cache

--- app-alwaysdata.js
+++ app-alwaysdata.js
@@ -5,8 +5,6 @@ const cors = require('cors');
 const { errorHandlingMiddleware, expressJsonMiddleware } = require('./middlewares/main.middlewares');
 const timeout = require('connect-timeout');
 const { IP_ALWAYS_DATA, PORT_ALWAYS_DATA } = require('./constants/main.constants');
-const cluster = require('cluster');
-const os = require('os');
 
 const app = express();
 
@@ -28,19 +26,4 @@ const startServer = async () => {
     }
 };
 
-if (cluster.isMaster) {
-    const numCPUs = os.cpus().length;
-    console.log(`Master ${process.pid} is running`);
-    
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
-    
-    cluster.on('exit', (worker) => {
-        console.log(`Worker ${worker.process.pid} has exited`);
-        cluster.fork();
-    });
-} else {
-    startServer();
-    console.log(`Worker ${process.pid} has started`);
-}
+startServer();
